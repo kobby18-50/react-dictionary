f6be7df1359fc82eb5ae8eb347cc0bedc53c26d1
Kwabena <79258511+kobby18-50@users.noreply.github.com>
2023-05-24 14:55:58 +0000

    updates

--- src/components/Main/HomePage.jsx
+++ src/components/Main/HomePage.jsx
@@ -111,8 +111,6 @@ const HomePage = () => {
                         </div>
                     ))
                 }
-                
-                
             </div>
           </div>
         ))}
